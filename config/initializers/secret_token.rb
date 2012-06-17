9a286dd06b172d21ecccb4156fddfe1f78fb75f9
Marc Beitchman <mbeitchman@gmail.com>
2012-06-17 16:42:47 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemini::Application.config.secret_token = '3f296e6f33f3418109c2dbd991e89f66750c0d0828265ffa48e34ec3d4310cd811b949b80f2f3c5b6c2bd03a150053c7ad7ccd0a419dee3bc0cae982c0033d25'
