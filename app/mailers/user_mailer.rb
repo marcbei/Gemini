999fd665902ee83d6deefed8a9df00bc2fb40945
Marc Beitchman <mbeitchman@gmail.com>
2012-06-24 20:05:20 -0700

    Fix emailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => "mbeitchman@gmail.com"
 
   def signup_email(user)
-    @user = user 
+    @interested_user = user 
     mail(:to => "mbeitchman@gmail.com,fcarlson@gmail.com", :subject => "New Gemini Interested User Sign-Up")
   end
 end
