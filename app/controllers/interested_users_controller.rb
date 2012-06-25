1a1b8d028faec1dd84ec2d678bf69425e04c4a02
Marc Beitchman <mbeitchman@gmail.com>
2012-06-24 19:51:58 -0700

    Updates

--- app/controllers/interested_users_controller.rb
+++ app/controllers/interested_users_controller.rb
@@ -50,7 +50,7 @@ class InterestedUsersController < ApplicationController
         flash[:success] = "Thank you for signing up!"
 
       else
-        format.html { redirect_to root_path, :notice => 'Please enter a valid name and email' }
+        format.html { redirect_to root_path, :notice => 'Please enter a valid name (first and last) and email address.' }
         format.json { render :json => @interested_user.errors, :status => :unprocessable_entity }
       end
     end
