bac1cd2e842eb8cbdecd7388ac418a301e784b6d
Marc Beitchman <mbeitchman@gmail.com>
2012-07-05 19:28:56 -0700

    LO-FI updates-8

--- app/models/interested_user.rb
+++ app/models/interested_user.rb
@@ -4,8 +4,8 @@ class InterestedUser < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
 
   VALID_NAME_REGEX  = /[\w]+([\s]+[\w]+){1}+/
-  validates :name, :presence=> true, :length=> { :maximum=> 75 }#, :format=> { :with=> VALID_NAME_REGEX }
-  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence=> true, :length=> { :maximum=> 75 }, :format=> { :with=> VALID_NAME_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence=> true, :format=> { :with=> VALID_EMAIL_REGEX },
                     :uniqueness=> true
 end
