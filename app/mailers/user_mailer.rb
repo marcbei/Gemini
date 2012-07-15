fafed7f39ed9bd2f0249f4c4bd0df0b79ab3f091
Marc Beitchman <mbeitchman@gmail.com>
2012-07-15 13:27:29 -0700

    Lo-fi updates5

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "info@vizibid.com"
+  default :from => "\"The Vizibid Team\" <info@vizibid.com>"
 
   def signup_email(user)
     @interested_user = user 
